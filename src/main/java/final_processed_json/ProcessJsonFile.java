686b450d94b9f5bd5fa4b2be1c35c2ef85626733
Mohamed Saeed <42535636+MohamedSci@users.noreply.github.com>
2024-03-01 11:53:45 +0200

    get total ayahs in Quran

--- src/main/java/final_processed_json/ProcessJsonFile.java
+++ src/main/java/final_processed_json/ProcessJsonFile.java
@@ -1,12 +1,14 @@
-package generate_keys_from_map_list;
+package final_processed_json;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import org.json.JSONArray;
+import java.io.FileWriter;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,14 +21,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class generate_keys_from_map_list {
-    String jsonFilePath = "C:\\Users\\moham\\OneDrive\\Desktop\\zekr\\single_zekr_dataset\\single_zekr_dataset.json"; // Path to your JSON file
-    String csvFilePath = "C:\\Users\\moham\\OneDrive\\Desktop\\zekr\\single_zekr_dataset\\single_zekr_dataset_keys.csv"; // Path to your output CSV file
-
+public class ProcessJsonFile {
     public static void main(String[] args) {
+        String inputFilePath = "C:\\Users\\moham\\OneDrive\\Desktop\\zekr\\single_zekr_dataset\\single_zekr_dataset_key_grouping_arabic.json"; // Path to your input JSON file
+        String outputFilePath = "C:\\Users\\moham\\OneDrive\\Desktop\\zekr\\single_zekr_dataset\\single_zekr_dataset_ffffffff_key_grouping_arabic.json"; // Path to your output JSON file
+
         try {
             // Read JSON file
-            FileReader reader = new FileReader("your_file_path.json");
+            FileReader reader = new FileReader(inputFilePath);
             JSONObject jsonObject = new JSONObject(reader);
             reader.close();
 
@@ -47,7 +49,7 @@ public class generate_keys_from_map_list {
             jsonObject.put("other", otherList);
 
             // Write the modified JSON object back to the file
-            FileWriter writer = new FileWriter("your_output_file.json");
+            FileWriter writer = new FileWriter(outputFilePath);
             writer.write(jsonObject.toString(4)); // Indented output for better readability
             writer.close();
         } catch (IOException | JSONException e) {
